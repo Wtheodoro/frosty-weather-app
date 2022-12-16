042fd1e0ef2ffa651be35e5cc15a82b99176849f
Walison Theodoro <theodoro.walison@gmail.com>
2022-12-16 20:17:34 -0300

    feat: add wheater main icons

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,27 @@ import LogoStorm from './svgs/LogoStorm'
 import LogoRain from './svgs/LogoRain'
 import Button from './Button'
 import CityPicker from './CityPicker'
+import WeatherCard from './WeatherCard/indext'
+import ClearDay from './svgs/ClearDay'
+import Wind from './svgs/Wind'
+import Humidity from './svgs/Humidity'
+import Cloud from './svgs/Cloud'
+import Thunder from './svgs/Thunder'
+import Drizzle from './svgs/Drizzle'
+import Snow from './svgs/Snow'
 
-export { PagehigherOrderComponent, LogoStorm, LogoRain, Button, CityPicker }
+export {
+  PagehigherOrderComponent,
+  LogoStorm,
+  LogoRain,
+  Button,
+  CityPicker,
+  WeatherCard,
+  ClearDay,
+  Wind,
+  Humidity,
+  Cloud,
+  Thunder,
+  Drizzle,
+  Snow,
+}
