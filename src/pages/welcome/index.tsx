3ec0b6ec931ab87ce0b26b9fb0f7d7ea7aa675b6
Walison Theodoro <theodoro.walison@gmail.com>
2022-12-16 10:44:06 -0300

    feat: welcome page

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import { PagehigherOrderComponent } from '..'
+import { Button, LogoStorm, LogoRain } from '../../components'
+import {
+  Container,
+  LogoWrapper,
+  VanishLeftLogo,
+  VanishRightLogo,
+} from './styles'
 
 const Welcome = () => {
-  return <div>Welcome</div>
+  return (
+    <Container>
+      <div>
+        <LogoWrapper>
+          <VanishLeftLogo>
+            <LogoStorm />
+          </VanishLeftLogo>
+
+          <VanishRightLogo>
+            <LogoRain />
+          </VanishRightLogo>
+        </LogoWrapper>
+      </div>
+
+      <h1>Discover the Weather in Your City</h1>
+
+      <p>Get to know your weather maps and radar preciptation forecast</p>
+
+      <Button>Get Started</Button>
+    </Container>
+  )
 }
 
-export default Welcome
+export default PagehigherOrderComponent(Welcome)
---
+++ src/pages/welcome/styles.tsx
@@ -0,0 +1,94 @@
+import styled, { keyframes } from 'styled-components'
+import { LogoStorm } from '../../components'
+
+export const Container = styled.main`
+  max-width: 400px;
+  height: 60vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  h1,
+  p {
+    text-align: center;
+    margin-top: 20px;
+  }
+
+  svg {
+    width: 200px;
+    height: 220px;
+  }
+
+  > div {
+    position: relative;
+    width: 210px;
+    height: 230px;
+  }
+
+  > button {
+    margin-top: 50px;
+  }
+`
+
+const LogoAnimation = keyframes`
+  0% {
+    transform: translateX(10px);
+  }
+
+  40% {
+    transform: translateX(10px);
+  }
+
+  60% {
+    transform: translateX(-390px);
+  }
+
+  100% {
+    transform: translateX(-390px);
+  }
+`
+
+export const LogoWrapper = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  width: 600px;
+  animation: ${LogoAnimation} 8s infinite alternate;
+`
+
+const LeftVanishAnimation = keyframes`
+  0% {
+    opacity: 1;
+  }
+
+  60% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+  }
+`
+
+export const VanishLeftLogo = styled.div`
+  animation: ${LeftVanishAnimation} 8s infinite alternate;
+`
+
+const RightVanishAnimation = keyframes`
+  0% {
+    opacity: 0;
+  }
+
+  60% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
+`
+
+export const VanishRightLogo = styled.div`
+  animation: ${RightVanishAnimation} 8s infinite alternate;
+`
