a2cc1f794bc7a0f1c832135cb2180dfb912b910b
Walison Theodoro <theodoro.walison@gmail.com>
2022-12-16 20:17:48 -0300

    fix: data unity

--- src/services/weather-services.ts
+++ src/services/weather-services.ts
@@ -3,7 +3,7 @@ import API_URL from './api'
 const weatherServices = {
   getCityWeather: async (city: string) =>
     await fetch(
-      `${API_URL}/weather?appid=${process.env.REACT_APP_API_KEY}&q=${city}`
+      `${API_URL}/weather?appid=${process.env.REACT_APP_API_KEY}&q=${city}&units=metric`
     ).then((response) => response.json()),
 }
 
