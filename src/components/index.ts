fda8bb5b87c35bb7d86b5bc68c715b49e94d48d8
Walison Theodoro <theodoro.walison@gmail.com>
2022-12-16 11:19:12 -0300

    fix: button component folder name for vercel purposes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 import PagehigherOrderComponent from './PagehigherOrderComponent'
 import LogoStorm from './svgs/LogoStorm'
 import LogoRain from './svgs/LogoRain'
-import Button from './Button'
+import Button from './button'
 
 export { PagehigherOrderComponent, LogoStorm, LogoRain, Button }
