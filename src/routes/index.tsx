6251c6d364042d8d3f5c7e3fab42fd7b62252c67
Walison Theodoro <theodoro.walison@gmail.com>
2022-12-22 18:35:18 -0300

    fix: add lezy loading on routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,49 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
-import { Home, Welcome, ChooseCity, Info } from '../pages'
+
+const Home = React.lazy(() => import('../pages/home'))
+const Welcome = React.lazy(() => import('../pages/welcome'))
+const ChooseCity = React.lazy(() => import('../pages/chooseCity'))
+const Info = React.lazy(() => import('../pages/info'))
 
 const AppRoutes: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Welcome />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/chooseCity' element={<ChooseCity />} />
-        <Route path='/info' element={<Info />} />
+        <Route
+          path='/'
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Welcome />
+            </Suspense>
+          }
+        />
+
+        <Route
+          path='/home'
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Home />
+            </Suspense>
+          }
+        />
+
+        <Route
+          path='/chooseCity'
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <ChooseCity />
+            </Suspense>
+          }
+        />
+        <Route
+          path='/info'
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Info />
+            </Suspense>
+          }
+        />
       </Routes>
     </BrowserRouter>
   )
